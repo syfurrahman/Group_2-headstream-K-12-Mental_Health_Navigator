d74be4962f939d196fd390b4c7d4d3562edb69d9
Tamanna Chowdhury <tamanna.chowdhury610@gmail.com>
2025-04-03 13:02:43 -0400

    completed

--- client/src/App.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import Home from "./pages/Home"; // adjust path if needed
-import "./styles.css"; // import global CSS
-
-function App() {
-  return <Home />;
-}
-
-export default App;
